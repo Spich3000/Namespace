d6537af2d61a0f24e397e3da0ff8f8677bd3826e
Spich <spichakovda@gmail.com>
2023-08-30 03:16:14 +0300

    Initial Commit

---
+++ Namespace/NamespaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  NamespaceApp.swift
+//  Namespace
+//
+//  Created by Дмитрий Спичаков on 30.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct NamespaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
